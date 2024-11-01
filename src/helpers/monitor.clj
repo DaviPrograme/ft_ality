c53094ab10fd959714652f94402bf5fd71a4c78e
Parrot User <user@localhost.localdomain>
2024-11-01 23:01:24 +0100

    a função monitor-sequence usa agora o seq-time para deterrmnarr o tempo de esperra do comando

--- src/helpers/monitor.clj
+++ src/helpers/monitor.clj
@@ -1,5 +1,5 @@
 (ns helpers.monitor
-    (:require [helpers.defs :refer [commands mls-sequence-time]]))
+    (:require [helpers.defs :refer [commands]]))
 
 
 (defn all-commnds-part-sequence? [cmds seq-time]
@@ -8,6 +8,5 @@
 (defn monitor-sequence [cmds-old-list seq-time]
     (let [cmds-now-list @commands]
         (when (and (not (= (count cmds-now-list) (count cmds-old-list))) (all-commnds-part-sequence? cmds-now-list seq-time))
-            (println "loop monitor-sequence")
-            (Thread/sleep mls-sequence-time)
+            (Thread/sleep seq-time)
             (recur cmds-now-list seq-time))))
