b1ee0a5a5c063f2af74c8aa32d9b01fcc8b3de76
Parrot User <user@localhost.localdomain>
2024-11-02 22:04:35 +0100

    criei a função have-more-elements-added? que verifica se maiis ccomandos foram adicionados na lista de comandos

--- src/helpers/monitor.clj
+++ src/helpers/monitor.clj
@@ -5,8 +5,11 @@
 (defn all-commnds-part-sequence? [cmds seq-time]
     (every? #(<= (- (:time (second %)) (:time (first %))) seq-time) (partition 2 1 cmds)))
 
+(defn- have-more-elements-added? [cmds-now cmds-old]
+    (not (= (count cmds-now) (count cmds-old))))
+
 (defn monitor-sequence [cmds-old-list seq-time]
     (let [cmds-now-list @commands]
-        (when (and (not (= (count cmds-now-list) (count cmds-old-list))) (all-commnds-part-sequence? cmds-now-list seq-time))
+        (when (and (have-more-elements-added? cmds-now-list cmds-old-list) (all-commnds-part-sequence? cmds-now-list seq-time))
             (Thread/sleep seq-time)
             (recur cmds-now-list seq-time))))
