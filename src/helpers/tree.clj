bda5147a71ba3119ae7d00755ea932d27ec6ab7d
Parrot User <user@localhost.localdomain>
2024-11-04 15:51:18 +0100

    dando push pq pode dar ruim no sistema operacional após a atualização que vou fazer. que deus me proteja!

--- src/helpers/tree.clj
+++ src/helpers/tree.clj
@@ -1,6 +1,7 @@
 (ns helpers.tree 
     (:require 
-        [helpers.aux :refer [get-sections remove-empty-lines get-part-list]]))
+        [helpers.aux :refer [get-sections remove-empty-lines get-part-list normalize-spaces]]
+        [clojure.string :as str]))
 
 
 (defn create-node [key name]
@@ -22,3 +23,19 @@
     (let [keys (get-part-list (remove-empty-lines (nth (get-sections content) 0)) 0)
           strokes (get-part-list (remove-empty-lines (nth (get-sections content) 0)) 1)]
         (zipmap strokes keys)))
+
+(defn- filter-empty-string [list]
+    (filter seq list))
+
+(defn- treat-combined-strike [strike]
+    (set (filter-empty-string  (str/split strike #"+"))))
+
+(defn build-combo-map [combo]
+    (let [name-combo  (normalize-spaces (get-part-list combo 0))
+          combo-stroke  (map #(if (str/includes? % "+") (treat-combined-strike %) %) (filter-empty-string (str/split (normalize-spaces (get-part-list combo 1)) #",")))]
+          combo-stroke))
+
+(defn build-tree [content]
+    (let [combos-list (remove-empty-lines (nth (get-sections content) 1))
+          strokes-map (strokes-commands-map content)]
+        (println (nth combos-list 0))))
