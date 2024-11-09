56d68143c8bcdc0877f16c2684beb3c8ce8ceb0e
Parrot User <user@localhost.localdomain>
2024-11-09 14:55:33 +0100

    fiz ajustes no moniitor.clj porque notei que não estava identificando a sequencia corrretamente

--- src/helpers/monitor.clj
+++ src/helpers/monitor.clj
@@ -1,26 +1,32 @@
 (ns helpers.monitor
-    (:require [helpers.defs :refer [commands is-run? mls-sequence-time]]))
+    (:require [helpers.defs :refer [commands is-run? mls-sequence-time key-waiting-time]]))
 
 
 (defn all-commnds-part-sequence? [cmds seq-time]
-    (every? #(<= (- (:time (second %)) (:time (first %))) seq-time) (partition 2 1 cmds)))
+    (every? #(<= (Math/abs (- (:time (second %)) (:time (first %)))) seq-time) (partition 2 1 cmds)))
 
 (defn- have-more-elements-added? [cmds-now cmds-old]
     (not (= (count cmds-now) (count cmds-old))))
 
 (defn count-commands-next-sequence [index cmds-list seq-time]
-    (if (or (>= (inc index) (count cmds-list)) (> (- (:time (nth cmds-list (inc index))) (:time (nth cmds-list index))) seq-time))
+    (if (or (>= (inc index) (count cmds-list)) (> (Math/abs (- (:time (nth cmds-list (inc index))) (:time (nth cmds-list index)))) seq-time))
         (inc index)
         (recur (inc index) cmds-list seq-time)))
 
-(defn wait-end-sequence [cmds-old-list seq-time]
-    (let [cmds-now-list @commands]
-        (when (and is-run? (have-more-elements-added? cmds-now-list cmds-old-list) (all-commnds-part-sequence? cmds-now-list seq-time))
-            (Thread/sleep seq-time)
-            (recur cmds-now-list seq-time))))
+
+(defn wait-end-sequence [seq-time]
+    (let [cmds-now-list @commands
+          now-time-millis (System/currentTimeMillis)
+          last-element-part? (<= (Math/abs (- now-time-millis (get (last cmds-now-list) :time 0))) (* 4 seq-time))
+          empty-list? (empty? cmds-now-list)]
+        (if (not (and @is-run? (or (empty? cmds-now-list) last-element-part?)))
+            cmds-now-list
+            (do
+                (Thread/sleep seq-time)
+                (recur seq-time)))))
 
 (defn count-keys-pressed-same-time [count key-focus keys-list time]
-    (if (or (empty? keys-list) (> (- (:time (first keys-list )) (:time key-focus)) time))
+    (if (or (empty? keys-list) (> (Math/abs (- (:time (first keys-list )) (:time key-focus))) time))
         count
         (recur (inc count) key-focus (drop 1 keys-list) time)))
 
@@ -42,9 +48,12 @@
 (defn monitor-commands []
     (while (or @is-run? (not (empty? @commands)))
         (do
-            (wait-end-sequence [] mls-sequence-time)
-            (when (not (empty? @commands))
-                (let [cmds-list @commands
-                      count-cmds-sequence (count-commands-next-sequence 0 cmds-list mls-sequence-time)
-                      cmds-sequence (take count-cmds-sequence cmds-list)]
-                    (reset! commands (drop count-cmds-sequence @commands)))))))
+            (let [cmds-list (sort-by :time (wait-end-sequence mls-sequence-time))]
+                (when (not (empty? cmds-list))
+                    (let [count-cmds-sequence (count-commands-next-sequence 0 cmds-list mls-sequence-time)
+                          cmds-sequence (take count-cmds-sequence cmds-list)
+                          handle-cmds-sequence (handle-commands [] cmds-list key-waiting-time)]
+                        ;; (println "ORIGINAL: " cmds-list )
+                        ;; (println "TAKE: " cmds-sequence )
+                        ;; (println "HANDLE: " handle-cmds-sequence "\n\n")
+                        (reset! commands (drop count-cmds-sequence @commands))))))))
